e7e8dbb838688590fb42fab67becbfb553bbcfef
morpheu <153627592+Morpheusmmt@users.noreply.github.com>
2025-02-14 12:54:02 +0000

    config: corrigir configuração do Vite para produção

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,29 @@ import react from "@vitejs/plugin-react";
 import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: "./", 
+
   plugins: [
     react(),
     themePlugin(),
     process.env.NODE_ENV === "development" && runtimeErrorOverlay(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
+
   build: {
-    outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
+    outDir: path.resolve(__dirname, "dist"), 
+    emptyOutDir: true, 
   },
 });
